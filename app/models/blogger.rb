0dd6e5174d08865d58a888b30cab8437e8363681
Eric Ou <eric.ou@yale.edu>
2020-07-04 12:17:11 -0700

    Done.

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,24 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts
 
+    validates :name, uniqueness: true
+    validates :age, numericality: {greater_than: 0}
+    validates :bio, length: {minimum: 30}
+
+    def total_likes
+        self.posts.reduce(0){|total, post| total + post.likes}
+    end
+
+    def most_liked_post
+        self.posts.max_by {|p| p.likes}
+    end
+
+    def top_five_destinations
+        a = self.destinations.group(:name).count.sort_by{|k,v| 
+            -v
+        }.map{|v| 
+            v[0]
+            }[0..4]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :bloggers, only: [:show, :new, :create]
+  resources :destinations, only: [:show]
+  resources :posts, only: [:show, :new, :create, :edit, :update]
+  post '/likes', to: 'posts#likes', as: "post_likes"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
